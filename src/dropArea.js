11a18791fac0cdfc06c2b90499b6ff1e5daea6f4
fsship <1844695939@qq.com>
2016-03-25 19:49:42 +0800

    load level from local file added

---
+++ src/dropArea.js
@@ -0,0 +1,27 @@
+'use strict';
+
+export default class DropArea {
+    constructor() {
+        this.element = document.createElement('div');
+        this.element.innerHTML = '拖放关卡文件到这里';
+        this.element.classList.add('dropArea');
+        this.element.dr
+        this.element.style.display = 'none';
+        document.body.appendChild(this.element);
+    }
+
+    show() {
+        this.element.style.display = 'block';
+    }
+
+    hide() {
+        this.element.style.display = 'none';
+    }
+
+    addDropHandler(callback) {
+        this.element.addEventListener('dragover', (e) => {
+            e.preventDefault();
+        });
+        this.element.addEventListener('drop', callback);
+    }
+}
